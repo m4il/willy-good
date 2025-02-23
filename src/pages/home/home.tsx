30e45c68dc0f32c44b2531c82621b4a6e66e1f40
William Hayward <haywardhwill@gmail.com>
2025-02-23 12:11:11 -0800

    test

--- src/pages/home/home.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Home = () => {
-    return <div className="home">
-        Welcome to my website. Content is loading... Stay tuned!
-    </div>
-}
-
-export default Home
